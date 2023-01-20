9234c144b323ea2918eec85d45e5baa95e63d6b7
jose.regalado <jose.regalado@irapuato.gob.mx>
2023-01-20 14:30:26 -0600

    Ejercicios controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,37 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(PageController::class)->group(function () {
+    Route::get('home',          'home')->name('home');
+    Route::get('blog',          'blog')->name('blog');
+    Route::get('blog/{slug}',   'post')->name('post');
 });
-
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
-
-Route::get('blog', function () {
-    $posts = [
-        [
-            'id' => 1,
-            'title' => 'PHP',
-            'slug' => 'php',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Laravel',
-            'slug' => 'laravel',
-        ]
-    ];
-
-    return view('blog', [
-        'posts' => $posts
-    ]);
-})->name('blog');
-
-Route::get('blog/{slug}', function ($slug) {
-    $post = $slug;
-
-    return view('post', [
-        'post' => $post
-    ]);
-})->name('post');
