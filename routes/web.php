4bf46614830ef31d85bfd39935c5b91ab358457b
Jose Juan Regalado <jregaladomoncada@gmail.com>
2023-01-20 07:43:49 -0600

    Cambios ejercicio

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('blog', function () {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'PHP',
+            'slug' => 'php',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Laravel',
+            'slug' => 'laravel',
+        ]
+    ];
+
+    return view('blog', [
+        'posts' => $posts
+    ]);
+})->name('blog');
+
+Route::get('blog/{slug}', function ($slug) {
+    $post = $slug;
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->name('post');
